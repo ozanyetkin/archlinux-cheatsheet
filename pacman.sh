3378076526a66c52dea495f1e8505de75b917ff0
Ozan Yetkin <33118911+ozanyetkin@users.noreply.github.com>
2023-01-17 22:47:51 +0300

    Create pacman.sh

---
+++ pacman.sh
@@ -0,0 +1,8 @@
+# Update and upgrade via pacman
+sudo pacman -Syu
+
+# Installing new package via pacman
+sudo pacman -S [packagename]
+
+# Removing a package via pacman
+sudo pacman -R [packagename]
