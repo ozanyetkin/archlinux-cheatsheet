043f320b42cd6949e2379c63eac2fb2ef8587bc4
Ozan Yetkin <33118911+ozanyetkin@users.noreply.github.com>
2023-01-26 13:13:09 +0300

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,8 +1,14 @@
 # Update and upgrade via pacman
 sudo pacman -Syu
 
+# Force update and upgrade via pacman
+sudo pacman -Syyu
+
 # Installing new package via pacman
 sudo pacman -S [packagename]
 
 # Removing a package via pacman
 sudo pacman -R [packagename]
+
+# Installing package with yay
+yay -S [packagename]
